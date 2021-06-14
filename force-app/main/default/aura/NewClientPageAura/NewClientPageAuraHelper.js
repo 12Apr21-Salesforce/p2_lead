9d1c44e0438ba978b825abc982bbc47bcefc18a9
Miguel McCrary <migmccrary@gmail.com>
2021-06-13 20:42:07 -0500

    retrieved and fix alerts

--- force-app/main/default/aura/NewClientPageAura/NewClientPageAuraHelper.js
+++ force-app/main/default/aura/NewClientPageAura/NewClientPageAuraHelper.js
@@ -12,29 +12,4 @@
         component.set("v.checkAgentListTrue", !component.get("v.checkAgentListTrue"));
     },
 
-    /*handleNavigate: function(cmp, event, helper) {
-        var navService = cmp.find("navService");
-        var pageReference = cmp.get("v.pageReference");
-        event.preventDefault();
-        navService.navigate(pageReference);
-    },*/
-
-        /*gotoAgentList:function(component,event,helper){
-            console.log('Enter Here');
-            var evt = $A.get("e.force:navigateToComponent");
-            console.log('evt'+evt);
-            evt.setParams({
-                componentDef: "c:AgentList"
-
-            });
-        evt.fire();
-        },*/
-
-        /*clear : function (component) {
-            component.find('clientfields').forEach(function(f) {
-            f.reset();
-            })
-            },*/
-    
-
 })
